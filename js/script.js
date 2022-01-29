176264e17c660eb95f9e2118961fac98d0d1d33d
DavideVin01 <davidevinciguerradv01@gmail.com>
2022-01-29 11:28:50 +0100

    Adds tasks

--- js/script.js
+++ js/script.js
@@ -1,2 +1,32 @@
 // console.log('Vue Ready', Vue);
+Descrizione:
+// Rifare l'esercizio della to do list.
+// Ogni todo sarà un oggetto, formato da due proprietà:
+// - text, una stringa che indica il testo del todo
+// - done, un booleano (true/false) che indica se il todo è stato fatto oppure no
+// MILESTONE 1
+// Stampare all'interno di una lista, un item per ogni todo.
+// Se la proprietà done è uguale a true, visualizzare il testo del todo sbarrato.
+// MILESTONE 2
+// Visualizzare a fianco ad ogni item ha una "x": cliccando su di essa, il todo viene rimosso dalla lista.
+// MILESTONE 3
+// Predisporre un campo di input testuale e un pulsante "aggiungi": cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
+
 Vue.config.devtools = true;
+
+const root = new Vue({
+    el: '#root',
+    data: {
+        task:[
+            {text: 'Lavare i piatti', done: false},
+            {text: 'Portare fuori il cane', done: false},
+            {text: 'Studiare', done: false},
+            {text: 'Stendere i panni', done: false},
+            {text: 'Fare la spesa', done: false},
+        ],
+    },
+    methods: {
+        
+    }
+});
